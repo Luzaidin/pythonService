8a2262035d16746d17830619b7b77937c7c47c9b
Luis Belo <luzaidinbelo@gmail.com>
2022-11-07 20:44:57 -0300

    remove: comments

--- main.py
+++ main.py
@@ -1,32 +1,3 @@
-"""
-# Prerequisites:
-pip3 install pywin32 pyinstaller
-
-# Build:
-pyinstaller.exe --onefile --runtime-tmpdir=. --hidden-import win32timezone myservice.py
-
-# With Administrator privilges
-# Install:
-dist\myservice.exe install
-
-# Start:
-dist\myservice.exe start
-
-# Install with autostart:
-dist\myservice.exe --startup delayed install
-
-# Debug:
-dist\myservice.exe debug
-
-# Stop:
-dist\myservice.exe stop
-
-# Uninstall:
-dist\myservice.exe remove
-
-
-"""
-
 import time
 import sys
 import logging
